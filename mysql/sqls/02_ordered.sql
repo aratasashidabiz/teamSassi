db38485081d99f143268879eed5f3f893f127e81
kotanomura <k.nomura@bizreach.co.jp>
2019-05-23 16:31:48 +0900

    insert文書いた

--- mysql/sqls/02_ordered.sql
+++ mysql/sqls/02_ordered.sql
@@ -3,7 +3,7 @@ USE mbshop;
 DROP TABLE IF EXISTS ordered;
 CREATE TABLE ordered (
     ordered_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_name VARCHAR(255) NOT NULL,
+    customer_name VARCHAR(255) NOT NULL,
     zip_code CHAR(8) NOT NULL,
     address VARCHAR(255) NOT NULL,
     telephone_number VARCHAR(15) NOT NULL,
@@ -11,13 +11,9 @@ CREATE TABLE ordered (
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('sassy', '123-2345', '沖縄', '090-2345-2345');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('どいっち', '323-3625', '大根県', '090-2345-2345');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('DON!じゃら', '333-3453', '金沢', '232-2755-2245');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('JOJO', '123-2345', '日本', '090-1111-2643');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('sassy', '123-2345', '沖縄', '090-2345-2345');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('おっくん', '323-3625', '山口', '090-2345-2345');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('ロンディー', '333-3453', '香港', '232-2755-2245');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('コウササン', '123-2345', 'アメリカ', '090-1111-2643');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('キッシー', '333-3453', 'カナダ', '232-2755-2245');
-INSERT INTO ordered(user_name, zip_code, address, telephone_number) VALUES ('益荒男', '123-2345', '火星', '090-1111-2643');
+INSERT INTO ordered(customer_name, zip_code, address, telephone_number) VALUES
+                ('sassy', '123-2345', '沖縄', '090-2345-2345'),('どいっち', '323-3625', '大根県', '090-2345-2345'),
+                ('DON!じゃら', '333-3453', '金沢', '232-2755-2245'),('JOJO', '123-2345', '日本', '090-1111-2643'),
+                ('sassy', '123-2345', '沖縄', '090-2345-2345'),('おっくん', '323-3625', '山口', '090-2345-2345'),
+                ('ロンディー', '333-3453', '香港', '232-2755-2245'),('コウササン', '123-2345', 'アメリカ', '090-1111-2643'),
+                ('キッシー', '333-3453', 'カナダ', '232-2755-2245'),('益荒男', '123-2345', '火星', '090-1111-2643');
