378ddbd5d004c5651c72a00f4de50ec6d6a4ec2e
Jotaro.Yuza <jotaro.yuza@bizreach.co.jp>
2019-05-24 11:07:59 +0900

    change src/sassi/dao/OrderDAO.java ver2

--- src/sassi/dao/OrderDAO.java
+++ src/sassi/dao/OrderDAO.java
@@ -11,29 +11,13 @@ import java.util.Map;
 
 public class OrderDAO {
 
-    // test
-    public static void main(String[] args) throws DAOException {
-        OrderDAO dao = new OrderDAO();
-        Map itemMap = new HashMap<Integer, ItemBean>();
-        ItemBean item1 = new ItemBean(3);
-        itemMap.put(1, item1);
-        ItemBean item2 = new ItemBean(4);
-        itemMap.put(2, item2);
-        CartBean cart = new CartBean();
-        cart.setItems(itemMap);
-//        cart.setCartNum(1);
-//        cart.setTotalPrice(1000);
-        CustomerBean customer = new CustomerBean("yea", "123-1223", "渋谷区", "090-0987-8765");
-        dao.saveOrder(customer, cart);
-    }
-
     private Connection connection;
 
     public OrderDAO() throws DAOException {
         getConnection();
     }
 
-    public int saveOrder(CustomerBean customer, CartBean cart)
+    public int saveOrdered(CustomerBean customer, CartBean cart)
             throws DAOException {
         if (connection == null)
             getConnection();
@@ -62,9 +46,11 @@ public class OrderDAO {
             }
             statement.close();
 
-            saveOrderedDetail();
+            // 注文情報のOrderedDetailテーブルへの追加
+            saveOrderedDetail(cart, lastInsertId);
 
             return lastInsertId;
+
         } catch (Exception e) {
             e.printStackTrace();
             throw new DAOException("レコードの操作に失敗しました。");
@@ -100,31 +86,54 @@ public class OrderDAO {
         }
     }
 
-    public int saveOrderedDetail(CartBean cart) {
-        // 注文情報のOrderedテーブルへの追加
-        String sql = "INSERT INTO ordered_detail(ordered_id, product_id, total_price, product_quantity) VALUES(?, ?, ?, ?)";
+    public int saveOrderedDetail(CartBean cart, int ordered_id) throws DAOException {
+        if (connection == null) getConnection();
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        int lastOrderedDetailId = 0;
 
-        Map itemMap = cart.getItems();
+        try {
+            // 注文情報のOrderedDetailテーブルへの追加
+            String sql = "INSERT INTO ordered_detail(ordered_id, product_id, total_price, product_quantity) VALUES(?, ?, ?, ?)";
 
-        Collection<ItemBean> values = itemMap.values();
-        for(ItemBean item: values) { // TODO 修正
-            PreparedStatement statement = connection.prepareStatement(sql);
-            // プレースホルダーの設定
-            statement.setInt(1, lastInsertId);
-            statement.setInt(2, item.getId());
-            statement.setInt(3, cart.getTotalPrice());
-            statement.setInt(4, cart.getCartNum());
+            Map itemMap = cart.getItems();
 
-            // SQLの実行
-            statement.executeUpdate();
+            Collection<ItemBean> values = itemMap.values();
+
+            for (ItemBean item : values) {
+                statement = connection.prepareStatement(sql);
+                // プレースホルダーの設定
+                statement.setInt(1, ordered_id);
+                statement.setInt(2, item.getId());
+                statement.setInt(3, cart.getTotalPrice()); // TODO ItemBeanが出来次第修正予定
+                statement.setInt(4, cart.getCartNum()); // TODO ItemBeanが出来次第修正予定
+
+                // SQLの実行
+                statement.executeUpdate();
+                resultSet = statement.executeQuery("SELECT LAST_INSERT_ID() AS LAST");
+                if (resultSet != null && resultSet.next()) {
+                    lastOrderedDetailId = resultSet.getInt("LAST");
+                }
+            }
+
+            statement.close();
+            return lastOrderedDetailId;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DAOException("レコードの操作に失敗しました。");
+        } finally {
+            try {
+                // リソースの開放
+                if (resultSet != null)
+                    resultSet.close();
+                if (statement != null)
+                    statement.close();
+                close();
+            } catch (Exception e) {
+                throw new DAOException("リソースの開放に失敗しました。");
+            }
         }
-
-
-//            resultSet = statement.executeQuery("SELECT LAST_INSERT_ID() AS LAST");
-//            if (resultSet != null && resultSet.next()) {
-//                lastInsertId = resultSet.getInt("LAST");
-//            }
-        statement.close();
     }
 
     private void close() throws SQLException {
@@ -133,5 +142,23 @@ public class OrderDAO {
             connection = null;
         }
     }
+
 }
 
+// test
+class TestOrderDAO {
+    public static void main(String[] args) throws DAOException {
+        OrderDAO dao = new OrderDAO();
+        Map itemMap = new HashMap<Integer, ItemBean>();
+        ItemBean item1 = new ItemBean(1, "インターステラー", 1200, "マシュー・マコノヒー", "クリストファーノーラン", "めっちゃいい", Timestamp.valueOf("2019-05-24 09:43:14"), Timestamp.valueOf("2019-05-24 09:43:14"));
+        ItemBean item2 = new ItemBean(2, "インターステラー2", 9999, "マシュー・マコノヒー2", "クリストファーノーラン2", "めっちゃいい2", Timestamp.valueOf("2019-05-24 09:43:14"), Timestamp.valueOf("2019-05-24 09:43:14"));
+        itemMap.put(1, item1);
+        itemMap.put(2, item2);
+        CartBean cart = new CartBean();
+        cart.setItems(itemMap);
+//        cart.setCartNum(1);
+//        cart.setTotalPrice(1000);
+        CustomerBean customer = new CustomerBean("first", "111-1111", "八王子市", "111-1111-1111");
+        dao.saveOrdered(customer, cart);
+    }
+}
